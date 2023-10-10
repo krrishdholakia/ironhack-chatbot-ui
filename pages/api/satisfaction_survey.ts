be1af1d431b52ac934c00b7421d9eb09c886bdd3
Sara Palacios <sara.palacios@ironhack.com>
2023-10-10 16:10:50 +0200

    feat(Satisfaction survey api): Adds PR changes

--- pages/api/satisfaction_survey.ts
+++ pages/api/satisfaction_survey.ts
@@ -1,41 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import NextCors from 'nextjs-cors';
 
-import verifyToken from '../../lib/auth';
-
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await NextCors(req, res, {
     methods: ['POST'],
     origin: '*',
   });
 
-  const { surveyType, responses } = req.body;
+  const { cohort_id, custom_params, responses, student_id, survey_type } =
+    req.body;
 
-  let session;
-
-  try {
-    const authorization = req?.headers?.authorization?.split(' ')[1];
-    session = await verifyToken(authorization);
-  } catch (error) {
-    return res.status(401).send({ error: 'You must be logged in.' });
-  }
-
-  await fetch(
-    `https://n8n-ih.herokuapp.com/webhook/${process.env.N8N_SATISFACTION_SURVEY_PATH}`,
-    {
-      method: 'POST',
-      headers: {
-        'X-API-Key': process.env.N8N_API_KEY as string,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        user_name: session.name,
-        user_email: session.email,
-        surveyType,
-        responses,
-      }),
+  await fetch(`${process.env.N8N_SATISFACTION_SURVEY_ENDPOINT}`, {
+    method: 'POST',
+    headers: {
+      'X-API-Key': process.env.N8N_API_KEY as string,
+      'Content-Type': 'application/json',
     },
-  );
+    body: JSON.stringify({
+      ...(custom_params && { custom_params: custom_params }),
+      cohort_id,
+      responses,
+      student_id,
+      survey_type,
+    }),
+  });
 
   res.status(200).send('OK');
 };
